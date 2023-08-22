b0f1df9f3e38125a0f2154675447bf8cd2b03155
mausao <mjampani@usp.br>
2023-08-22 18:00:58 -0300

    adding some content ab python

---
+++ python3/friends.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python3
+import sys
+friend1 = sys.argv[1] #pega o primeiro comando passado na linha
+friend2 = sys.argv[2] #pega o segundo comando passado na linha
+print(friend1, 'and', friend2, 'are friends') #retorna uma mensagem com as variáveis passadas
+
---
+++ python3/python1problemset/problem1.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python3
+import sys
+nome = sys.argv[1]
+cor = sys.argv[2]
+fav_atv = sys.argv[3]
+fav_anm = sys.argv[4]
+print("My name is: ", nome, "\nMy favorite color is: ", cor, "\nMy favorite activity is: ", fav_atv, "\nMy favorite animal is: ", fav_anm, "\nAnd this is my first python program with +4 variables")
