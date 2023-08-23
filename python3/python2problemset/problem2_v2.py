e3dc68ef2bcfbd5f7ccaf6f08dd2389f6709bf62
mausao <mjampani@usp.br>
2023-08-23 15:14:18 -0300

    adding the final answer of the python2

--- python3/python2problemset/problem2_v2.py
+++ python3/python2problemset/problem2_v2.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
+import sys
 try:
-    num = int(input("Digite um número: "))
+    num = int(sys.argv[1])
     even = num/2
     if num > 0:
-        print("O número digitado é positivo!")
+        print("O número é positivo")
         if num < 50:
             print("O número além de positivo é menor do que 50!")
             if even == int(even):
-                print("O numéro além de positivo e menor do que 50 também é par!")
+                print("O número além de positivo e menor do que 50 também é par!")
         elif num > 50:
             print("O número é maior do que 50!")
             if num % 3 == 0:
-                print("O número além de maior que 50 também é divisível por 3!")
+                print("O número além de maior de 50 também é divisível por 3!")
     elif num == 0:
         print("O número digitado é 0! Positivo ou negativo?")
-    else: 
+    else:
         print("O número digitado é negativo!")
 except ValueError:
     print("Oops! Digite um número válido!")
+
+
