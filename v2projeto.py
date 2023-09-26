d7c538301bf3a00cbdcffe6d841eba9ed3d8c66b
mausao <mjampani@usp.br>
2023-09-26 20:05:40 -0300

    Atualizando o script - Output: matrizes das páginas do .xlsx

--- v2projeto.py
+++ v2projeto.py
@@ -6,14 +6,13 @@ planilha = sys.argv[1]
 def mean_grouped(dados):
     mean_values = {}
     valores_tempo = {}
-    x = 1
     for linha in range(2, dados.max_row + 1):
         valor_tempo = dados.cell(row = linha, column = 2).value
         valor = dados.cell(row = linha, column = 3).value
         if valor_tempo is not None and valor is not None:
 #Criando/adicionando valores ao dicionário valores_tempo[] pela verificação se o dicionário contém a chave ou não
             valor = float(valor)
-            if valor_tempo in (0, 1, 2, 4, 8, 12):
+            if valor_tempo in (0, 1, 2, 4, 8):
                 if valor_tempo not in valores_tempo:
                     valores_tempo[valor_tempo] = [valor]
                 else:
@@ -31,28 +30,21 @@ def mean_grouped(dados):
 
 try:
     arquivo = openpyxl.load_workbook(planilha)
-    dados1 = arquivo['FvFm']
-    dados2 = arquivo['ChlIdx']
-    dados3 = arquivo['AriIdx']
     print(f"O arquivo foi aberto e salvo com sucesso!")
-    resultado1 = mean_grouped(dados1)
-    resultado2 = mean_grouped(dados2)
-    resultado3 = mean_grouped(dados3)
-    print(resultado)
-    for tempo in resultado1.keys():
-        print("As médias da primeira planilha (FvFm) são:")
-        print(tempo)
-        print(resultado1[tempo])
-    for tempo in resultado2.keys():
-        print("As médias da segunda planilha (ChlIdx) são: ")
-        print(tempo)
-        print(resultado2[tempo])
-    for tempo in resultado3.keys():
-        print("As médias da terceira planilha (AriIdx) são: ")
-        print(tempo)
-        print(resultado3[tempo])
-
-
+    pages_names = arquivo.sheetnames
+    matriz = [[0.0 for _ in range(5)] for _ in range(5)]
+    for name in pages_names:
+        print(f"Processando a página {name}")
+        dados = arquivo[name]
+        resultado = mean_grouped(dados)
+#loop para adicionar os valores do output da função em uma matriz 5x5
+        for i, tempo in enumerate(sorted(resultado.keys())):
+            medias = resultado[tempo]
+            matriz[i] = medias
+#loop para printar as linhas da matriz alimentada com os valores
+        for linha in matriz:
+            row_edit = [round(media, 3) for media in linha]
+            print(row_edit)
 
 except IOError as e:
     print(f"Erro ao ler o arquivo: {e}")
